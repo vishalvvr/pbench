6e97e6c4fd54c18a504df907c463a41b5275d873
vishalvvr <vishalvijayraghavan@gmail.com>
2022-10-14 13:02:12 +0530

    add new dashboard code

---
+++ docs/new_gh-pages/app.py
@@ -0,0 +1,22 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+import json
+import shutil
+
+with open('data.json') as fobj:
+    site_data = json.load(fobj)
+
+file_loader = FileSystemLoader("template")
+env = Environment(loader=file_loader)
+
+template = env.get_template('index.html').render(site_data)
+
+public_dir = os.path.join(os.getcwd(),"public")
+
+#if path already exists, remove it before copying with copytree()
+if os.path.exists(public_dir):
+    shutil.rmtree(public_dir)
+shutil.copytree(os.path.join(os.getcwd(),"template"), public_dir)
+
+with open(os.path.join(public_dir,"index.html"),"w") as fobj:
+    fobj.writelines(template)
